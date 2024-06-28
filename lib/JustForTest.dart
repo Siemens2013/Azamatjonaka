2513bff018361d402db67f6fe8297d5320ffb459
Siemens2013 <siemens.repair2013@gmail.com>
2024-06-27 20:39:08 -0400

    Learning listview

--- lib/JustForTest.dart
+++ lib/JustForTest.dart
@@ -12,6 +12,25 @@ class _JustForTestState extends State<JustForTest> {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: Text(widget.appbartxt,  textAlign: TextAlign.center,
       style: TextStyle(fontSize: 20, color: Colors.white),),
-      centerTitle: true, backgroundColor: Colors.lightBlueAccent,),);
+      centerTitle: true, backgroundColor: Colors.lightBlueAccent,),
+
+        body: ListView(
+    children: const <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+    ),
+              ListTile(
+                leading: Icon(Icons.photo_album),
+                 title: Text('Album'),
+    ),
+            ListTile(
+                leading: Icon(Icons.phone),
+              title: Text('Phone'),
+    ),
+    ],
+
+        ),
+    );
   }
 }
